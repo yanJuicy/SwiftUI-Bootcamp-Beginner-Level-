91847a73b35a9f039f40c2103e1c793b1af29e9c
yanJuicy <hanjub3@gmail.com>
2024-03-13 23:00:14 +0900

    How to use Xcode in swiftUI project

---
+++ SwiftThinkingBootcamp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftThinkingBootcamp
+//
+//  Created by a on 2024/03/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .font(.title)
+            .fontWeight(.black)
+            .foregroundColor(.green)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftThinkingBootcamp/SwiftThinkingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftThinkingBootcampApp.swift
+//  SwiftThinkingBootcamp
+//
+//  Created by a on 2024/03/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftThinkingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
